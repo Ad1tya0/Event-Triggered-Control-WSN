4cbca82c7e0103ed1342167ce4643236b23b326d
Ad1tya0 <adittobhct@gmail.com>
2022-11-10 01:41:58 +0100

    implement tree, start downward forwarding

--- flooding.c
+++ flooding.c
@@ -12,7 +12,7 @@
 //#define BEACON_INTERVAL (CLOCK_SECOND * 60) /* Try to change this period to analyse \
 //                                             * how it affects the radio-on time     \
 //                                             * (energy consumption) of ContikiMac */
-
+#define NUM_SENSOR 5 //cant retrieve this from app.c
 linkaddr_t etc_controllerx = {{0xF7, 0x9C}};//stupid method, should port node allocator from app.c, but if it works it works ig
 struct beacon_msg {
     uint16_t seqn;
@@ -28,13 +28,21 @@ struct connection_t {//use for making a buffer for connections?
     linkaddr_t parent;
 };
 
+
+struct forwardCollect_t {//header packet for data collection
+    linkaddr_t source;
+    uint8_t hops;
+} __attribute__((packed));
+
+
+
 struct event_msg_t {
     linkaddr_t event_source;
     uint16_t event_seqn;
 }__attribute__((packed));
 
 struct connection_t ConnectionBuffer[5];//store 5 best connections here, use some
-
+struct forwardCollect_t collect[NUM_SENSOR];// for data forwarding
 /* Callback function declarations */
 void bc_recv(struct broadcast_conn *conn, const linkaddr_t *sender);
 void uc_recv(struct unicast_conn *c, const linkaddr_t *from);
@@ -52,8 +60,8 @@ struct broadcast_callbacks bc_cb = {
 enum broadcast_msg_type {
     BC_TYPE_BEACON,
     BC_TYPE_EVENT,
-    BC_COLLECT_REQ,
-    BC_COLLECT_ACK
+    BC_FORWARD_REQ,
+    BC_FORWARD_ACK
 }__attribute__((packed));
 
 struct broadcast_header {
@@ -133,7 +141,7 @@ void beacon_Start(void){
 void beacon_Stop(void){
     for(int i=0; i<5; i++){ //its 5 because i take 5 connections in the connection buffer: ConnectionBuffer[5]
         ConnectionBuffer[i].seqn = 0;
-        ConnectionBuffer[i].metric = 0;
+        ConnectionBuffer[i].metric = 50;
         ConnectionBuffer[i].rssi = RSSI_THRESHOLD;
         ConnectionBuffer[i].parent = &linkaddr_null;
     }//remove all connections, wrap in function? might reuse
@@ -143,6 +151,103 @@ void beacon_Stop(void){
 void beacon_recv(struct broadcast_header *broadcastHeader, linkaddr_t *sender){ //use header enum instead of callback, should be useful to understand type of message easily
 
     struct beacon_msg beaconMsg;
-//do beacon tree logic with hops and rssi
-}
+    int16_t rssi;
+    int i,j =0;
+    //struct my_collect_conn *conn; //not gonna use this
+
+
+//    /* Get the pointer to the overall structure my_collect_conn from its field bc */
+//    conn = (struct my_collect_conn *)(((uint8_t *)bc_conn) -
+//                                      offsetof(struct my_collect_conn, bc));
+
+    /* Check if the received broadcast packet looks legitimate */
+    if (packetbuf_datalen() != sizeof(struct beacon_msg)) {
+        printf("my_collect: broadcast of wrong size\n");
+        return;
+    }
+    //memcpy(&beacon, packetbuf_dataptr(), sizeof(struct beacon_msg));
+    packetbuf_copyto(&beaconMsg); //this works as well
+
+    //Read the RSSI of the *last* reception.
+    rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+    printf("recv beacon from %02x:%02x seqn %u metric %u rssi %d\n",
+           sender->u8[0], sender->u8[1],
+           beacon.seqn, beacon.metric, rssi);
+
+    if (beaconMsg.seqn != 0 && rssi < RSSI_THRESHOLD || beaconMsg.seqn < ConnectionBuffer[0].seqn)
+        return;                             // The beacon is either too weak or too old, ignore it
+    if (beaconMsg.seqn == ConnectionBuffer[0].seqn) { // The beacon is not new, check the metric
+        for (i=0; i<5; i++){
+            if(beaconMsg.seqn == 0 && beaconMsg.seqn > ConnectionBuffer[0].seqn)
+                break;
+            if(beaconMsg.metric + 1 >= ConnectionBuffer[i].metric) // Worse or equal than what we have, ignore it
+                continue;   //more hops == bad
+            if(rssi <= ConnectionBuffer[i].rssi)    //worse or same rssi means we can try to find better connections
+                continue;   //more rssi == awyiss
+            break;
+        }
+        if (i >= 5)//cant store more than 5 connections
+            return;
+    }
+
+        for(j = 4; j>i; j--){    //put connections in buffer accordingly, updating current index with a better one if found
+            linkaddr_copy(&ConnectionBuffer[j].parent, &ConnectionBuffer[j-1].parent);
+            ConnectionBuffer[j].seqn = ConnectionBuffer[j-1].seqn;
+            ConnectionBuffer[j].metric = ConnectionBuffer[j-1].metric;
+            ConnectionBuffer[j].rssi = ConnectionBuffer[j-1].rssi;
+        }
+        for(j=0; j<5; j++){ //its 5 because i take 5 connections in the connection buffer: ConnectionBuffer[5]
+            linkaddr_copy(&ConnectionBuffer[j].parent, &linkaddr_null);
+            ConnectionBuffer[j].seqn = 0;
+            ConnectionBuffer[j].metric = 0;
+            ConnectionBuffer[j].rssi = RSSI_THRESHOLD;
+        }//remove all connections in that index
+
+        /* Otherwise, memorize the new parent, the metric, and the seqn, we also store rssi */
+        linkaddr_copy(&ConnectionBuffer[i].parent, sender);
+        ConnectionBuffer[i].metric = beaconMsg.metric + 1;
+        ConnectionBuffer[i].seqn = beaconMsg.seqn;
+        ConnectionBuffer[i].rssi = rssi;
+
+
+        /* TO DO 4://done
+* If the metric or the seqn has been updated, retransmit *after a small, random
+* delay* (BEACON_FORWARD_DELAY) the beacon message in broadcast, to update the
+* node's neighbors about the routing changes
+*/
+        if(i==0){//first index in buffer is the best possible connection, the next is the second best connection
+            /* Schedule beacon propagation */
+            ctimer_set(&beacon_timer, BEACON_FORWARD_DELAY, beacon_timer_cb, NULL);
+            printf("my_collect: new parent %02x:%02x, my metric %d, rssi %d\n",
+               ConnectionBuffer[0]->parent.u8[0], ConnectionBuffer[0].parent.u8[1], ConnectionBuffer[0].metric, ConnectionBuffer[0].rssi);
+        }
+        else{
+            printf("my_collect: second best parent %02x:%02x, my metric %d, rssi %d\n",
+                   ConnectionBuffer[i]->parent.u8[0], ConnectionBuffer[i].parent.u8[1], ConnectionBuffer[i].metric, ConnectionBuffer[i].rssi);
+        }
+    }
+
+struct connection_t *get_BestConnection(void){//call this function to get best possible connection
+        return &ConnectionBuffer[0];
+    }
+
+    /////////////////////////////////////////Downward Forwarding for actuation//////////////////////////////////////////
+
+    void downForward_Reset(void) {
+        for (int i = 0; i < NUM_SENSOR; i++) {
+            linkaddr_copy(&collect[i].source, &linkaddr_null);
+            collect[i].hops = 50;
+        }
+    }
+
+    struct forwardCollect_t downForward_Start(linkaddr_t* srcSensor, linkaddr_t* srcHop, uint8_t metric){
+    for(int i=0; i< NUM_SENSOR; i++){
+        if(linkaddr_cmp(srcSensor, &colect[i].source))
+            return &collect[i];
+        }
+    }
+
+
+
+
 
